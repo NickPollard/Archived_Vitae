863ee2dc9efba561219d03c4ebcc1807bce0cb37
Nick Pollard <nickedwardpollard@gmail.com>
2012-10-25 14:29:15 +0100

    Bunker asset tweaks.

--- dat/model/bunker.s
+++ dat/model/bunker.s
@@ -1,2 +1,2 @@
 (model (object_process (meshLoadFile "dat/model/bunker.obj" )
-			 (quote (attribute "diffuse_texture" "dat/img/brush_tile.tga"))))
+			 (quote (attribute "diffuse_texture" "dat/img/brush_tile_dark.tga"))))
